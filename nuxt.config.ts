200d7623ba458e3dd6207d4602b1199eeb16d56f
Yurii Kovalchuk <failwin@ukr.net>
2025-12-05 10:33:14 +0200

    add firebase deploy config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'firebase_app_hosting',
+  },
+
   hooks: {
     'prerender:routes': ({ routes }) => {
       routes.clear()
